c1abb9b23121d5d591de431eddd685da4361fb7d
naufaladrian <naufal.adrian904@gmail.com>
2022-08-10 08:26:25 +0700

    select active contact

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [active, setActive] = useState(null);
+  const datauser = [
+    {
+      username: "bebas",
+      lastmassage: "Lorem ipsum dolor sit",
+    },
+    {
+      username: "bebas 2",
+      lastmassage: "Lorem ipsum dolor sit",
+    },
+  ];
+  const onSelect = (idx) => {
+    setActive(idx);
+  };
   return (
     <div className="container">
       <div className="side-bar">
@@ -31,114 +46,33 @@ function App() {
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
           </svg>
         </div>
-        <div className="contact side-content">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            className="bi bi-person-circle"
-            viewBox="0 0 16 16"
+        {datauser.map((e, i) => (
+          <div
+            className="contact side-content"
+            style={
+              active === i ? { backgroundColor: "#4da6ff", color: "white" } : {}
+            }
+            key={i}
+            onClick={() => onSelect(i)}
           >
-            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-            <path
-              fillRule="evenodd"
-              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
-            />
-          </svg>
-          <div className="contact-text">
-            <h4 className="contact-name">Lorem ipsum</h4>
-            <p className="last massage">Lorem ipsum dolor sit</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              className="bi bi-person-circle"
+              viewBox="0 0 16 16"
+            >
+              <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+              <path
+                fillRule="evenodd"
+                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+              />
+            </svg>
+            <div className="contact-text">
+              <h4 className="contact-name">{e.username}</h4>
+              <p className="last massage">{e.lastmassage}</p>
+            </div>
           </div>
-        </div>
-        <div className="contact side-content">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            className="bi bi-person-circle"
-            viewBox="0 0 16 16"
-          >
-            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-            <path
-              fillRule="evenodd"
-              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
-            />
-          </svg>
-          <div className="contact-text">
-            <h4 className="contact-name">Lorem ipsum</h4>
-            <p className="last massage">Lorem ipsum dolor sit</p>
-          </div>
-        </div>
-        <div className="contact side-content">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            className="bi bi-person-circle"
-            viewBox="0 0 16 16"
-          >
-            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-            <path
-              fillRule="evenodd"
-              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
-            />
-          </svg>
-          <div className="contact-text">
-            <h4 className="contact-name">Lorem ipsum</h4>
-            <p className="last massage">Lorem ipsum dolor sit</p>
-          </div>
-        </div>
-        <div className="contact side-content">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            className="bi bi-person-circle"
-            viewBox="0 0 16 16"
-          >
-            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-            <path
-              fillRule="evenodd"
-              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
-            />
-          </svg>
-          <div className="contact-text">
-            <h4 className="contact-name">Lorem ipsum</h4>
-            <p className="last massage">Lorem ipsum dolor sit</p>
-          </div>
-        </div>
-        <div className="contact side-content">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            className="bi bi-person-circle"
-            viewBox="0 0 16 16"
-          >
-            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-            <path
-              fillRule="evenodd"
-              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
-            />
-          </svg>
-          <div className="contact-text">
-            <h4 className="contact-name">Lorem ipsum</h4>
-            <p className="last massage">Lorem ipsum dolor sit</p>
-          </div>
-        </div>
-        <div className="contact side-content">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            className="bi bi-person-circle"
-            viewBox="0 0 16 16"
-          >
-            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-            <path
-              fillRule="evenodd"
-              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
-            />
-          </svg>
-          <div className="contact-text">
-            <h4 className="contact-name">Lorem ipsum</h4>
-            <p className="last massage">Lorem ipsum dolor sit</p>
-          </div>
-        </div>
+        ))}
       </div>
       <div className="main">
         <div className="top-bar">
